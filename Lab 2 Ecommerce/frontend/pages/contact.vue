f56436c7956afed3f727085f4cd1aed2056a78ff
DionOsmani <100315365+DionOsmani@users.noreply.github.com>
2023-04-25 22:37:44 +0200

    About Page MVP

--- Lab 2 Ecommerce/frontend/pages/contact.vue
+++ Lab 2 Ecommerce/frontend/pages/contact.vue
@@ -1,11 +1,14 @@
 <template>
-
+  <div>
+    <NuxtLayout name="client">
+      <template #header></template>
+      <template #footer></template>
+    </NuxtLayout>
+  </div>
 </template>
 
-<script>
-
+<script setup>
+  definePageMeta({
+    layout: false,
+  });
 </script>
-
-<style>
-
-</style>
