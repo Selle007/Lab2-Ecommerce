7cc0ae52f3e7273751bfeec32d07d98457818171
DionOsmani <100315365+DionOsmani@users.noreply.github.com>
2023-04-30 22:46:53 +0200

    Contact Form API

---
+++ Lab 2 Ecommerce/backend/backend/Controllers/ContactsController.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IMongoCollection<Contact> _contacts;
+
+        public ContactsController(IMongoClient client)
+        {
+            var database = client.GetDatabase("Lab2");
+            _contacts = database.GetCollection<Contact>("contacts");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contact>>> GetAll()
+        {
+            try
+            {
+                var contacts = await _contacts.Find(contact => true).ToListAsync();
+                return Ok(contacts);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+
+        [HttpGet("{id}", Name = "GetContact")]
+        public async Task<ActionResult<Contact>> GetById(string id)
+        {
+            try
+            {
+                var contact = await _contacts.Find<Contact>(c => c.Id == id).FirstOrDefaultAsync();
+
+                if (contact == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(contact);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Contact>> Create([FromBody] Contact contact)
+        {
+            try
+            {
+                contact.Id = ObjectId.GenerateNewId().ToString();
+                await _contacts.InsertOneAsync(contact);
+                return CreatedAtRoute("GetContact", new { id = contact.Id }, contact);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(string id, [FromBody] Contact contact)
+        {
+            try
+            {
+                var existingContact = await _contacts.Find<Contact>(c => c.Id == id).FirstOrDefaultAsync();
+
+                if (existingContact == null)
+                {
+                    return NotFound();
+                }
+
+                contact.Id = existingContact.Id;
+                await _contacts.ReplaceOneAsync(c => c.Id == id, contact);
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            try
+            {
+                var contact = await _contacts.Find<Contact>(c => c.Id == id).FirstOrDefaultAsync();
+
+                if (contact == null)
+                {
+                    return NotFound();
+                }
+
+                await _contacts.DeleteOneAsync(c => c.Id == id);
+
+                return NoContent();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Internal server error: {ex}");
+            }
+        }
+    }
+}
